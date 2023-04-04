7e7e8939ce8415f64fe20fffcca4c2e001f52931
JatoMixo <jatomixo@outlook.es>
2023-04-04 15:22:39 +0200

    made a card for storing tool information. started working on its animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Useless_Image from "./images/delete_this/linux.webp"
+  import ToolCard from "./components/ToolCard.svelte";
+
+  let description_for_dragon = "It's a dragon I downloaded from the Internet because I'm lazy. Please help.";
+</script>
+
+<ToolCard name="Dragon" description={description_for_dragon} image={Useless_Image}/>
---
+++ src/routes/components/ToolCard.svelte
@@ -0,0 +1,78 @@
+<script>
+  export let name = "";
+  export let image = "";
+  export let description = "";
+  export let link = "";
+</script>
+
+<style lang="scss">
+
+  @keyframes on_hover {
+    0% {top: 0px}
+    100% {top: -25px}
+  }
+
+  div {
+    /* BORDER */
+    border: solid 5px #ff5555;
+    border-radius: 10px;
+    box-shadow: 0 0 10px #ff5555;
+
+    /* SIZE */
+    width: 10%;
+    height: fit-content; // 260px
+
+    /* PADDING */
+    padding: 15px;
+
+    /* COLOR */
+    background-color: #1D1D1D;
+
+    /* IMAGE INSIDE */
+    img {
+      position: relative;
+
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+
+      height: 100%;
+      width: 100%;
+    }
+
+    :hover {
+      animation-name: on_hover;
+      animation-duration: 0.5s;
+    }
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  p {
+    font-size: 16px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 28px;
+  }
+</style>
+
+{#if link != ""}
+<div>
+  <a href={link}>
+    <img src={image} alt="Didn't load"/>
+    <h1>{name}</h1>
+    <p>{description}</p>
+  </a>
+</div>
+{:else}
+  <div>
+    <img src={image} alt="Didn't load"/>
+    <h1>{name}</h1>
+    <p>{description}</p>
+  </div>
+{/if}
