d0e44e4f9257007f2c7f0b40b67ff242981009dd
JatoMixo <jatomixo@outlook.es>
2023-04-07 15:29:19 +0200

    project card almost finished, only need to add backwards animation

--- src/routes/components/ToolCard.svelte
+++ src/routes/components/ToolCard.svelte
@@ -7,45 +7,33 @@
 
 <style lang="scss">
 
+  $default_box_width: 10%;
+
   @keyframes on_hover {
     0% {top: 0px}
     100% {top: -25px}
   }
 
-  div {
-    /* ANIMATION */
-    :hover > * {
-      position: relative;
+  .box {
+    width: fit-content;
+    height: fit-content;
 
-      animation-name: on_hover;
-      animation-duration: 0.5s;
-      animation-fill-mode: forwards;
-    }
-
-    /* BORDER */
-    border: solid 5px $secondary_red_color;
-    border-radius: 10px;
-    box-shadow: 0 0 10px $secondary_red_color;
-
-    /* SIZE */
-    width: 10%;
-    height: fit-content; // 260px
-
-    /* PADDING */
-    padding: 15px;
-
-    /* COLOR */
-    background-color: #1D1D1D;
-
-    /* IMAGE INSIDE */
     img {
-      display: block;
-      margin-left: auto;
-      margin-right: auto;
-
       height: 100%;
       width: 100%;
     }
+  }
+
+  .animation {
+    width: $default_box_width;
+  }
+
+  .animation:hover {
+    position: relative;
+
+    animation-name: on_hover;
+    animation-duration: 0.5s;
+    animation-fill-mode: forwards;
   }
 
   a {
@@ -63,7 +51,7 @@
   }
 </style>
 
-{#if link != ""}
+<!--{#if link != ""}
 <div>
   <a href={link}>
     <img src={image} alt="Didn't load"/>
@@ -77,4 +65,24 @@
     <h1>{name}</h1>
     <p>{description}</p>
   </div>
+{/if}-->
+
+{#if link == ""}
+<div class="animation">
+  <div class="box">
+    <img src={image} alt="Didn't load"/>
+    <h1>{name}</h1>
+    <p>{description}</p>
+  </div>
+</div>
+{:else}
+<div class="animation">
+  <a href={link}>
+    <div class="box">
+      <img src={image} alt="Didn't load"/>
+      <h1>{name}</h1>
+      <p>{description}</p>
+    </div>
+  </a>
+</div>
 {/if}
