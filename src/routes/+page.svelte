479f568412320e1545817e0462dc8ee45634d7b2
JatoMixo <jatomixo@outlook.es>
2023-04-08 13:14:41 +0200

    created row class to align items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,3 @@
   const DELETE_THIS = "It's a dragon I downloaded from the Internet because I'm lazy. Please help.";
 </script>
 
-<ToolCard name="Dragon" description={DELETE_THIS} image={Useless_Image} link="./delete_this" />
