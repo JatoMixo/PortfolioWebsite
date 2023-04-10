76fc48307f350f2015d22b4dee3fc0e21054fdaa
JatoMixo <jatomixo@outlook.es>
2023-04-10 17:15:48 +0200

    Made first logo/title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,39 @@
 <script>
-  import Useless_Image from "./images/delete_this/linux.webp"
   import ToolCard from "./components/ToolCard.svelte";
-
-  const DELETE_THIS = "It's a dragon I downloaded from the Internet because I'm lazy. Please help.";
+  import JatoMixoLogo from "./images/jatomixo/jatomixo_logo.png";
 </script>
+
+<style lang="scss">
+  .logo {
+    transform-origin: center;
+    justify-content: center;
+
+    text-align: center;
+
+    position: relative;
+
+    img {
+      width: 60%;
+      transform: translateY(7.5%);
+    }
+
+    h1 {
+      margin-top: 3vw;
+      margin-bottom: 3vw;
+
+      font-size: 6vw;
+      text-shadow: 0 0 5px white;
+    }
+  }
+</style>
+
+<!--Title/Logo-->
+<div class="logo">
+  <img src={JatoMixoLogo} alt="Didn't load correctly"/>
+  <h1>JatoMixo</h1>
+</div>
+
+<hr>
+
+<!--About-->
 
