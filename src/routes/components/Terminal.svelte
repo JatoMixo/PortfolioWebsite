d78919a76fa71ba99d8a5c5dc9dc3196cca65256
JatoMixo <jatomixo@outlook.es>
2023-04-22 19:49:12 +0200

    Finished Terminal Style

---
+++ src/routes/components/Terminal.svelte
@@ -0,0 +1,67 @@
+<script>
+  const leftArrow = "";
+  let command = "";
+
+  const correctCommandStatus = "#00FFCA";
+  const wrongCommandStatus = "#FF0000";
+
+  let lineStatus = correctCommandStatus;
+</script>
+
+<style lang="scss">
+  .box {
+    padding: 10px;
+    background-color: $secondary_background_color;
+
+    p {
+      margin: 0;
+    }
+
+    input {
+      width: 100%;
+
+      font-size: 75%;
+
+      color: whitesmoke;
+      font-family: "Hack Nerd Font";
+
+      border-width: 0px;
+
+      background-color: $secondary_background_color;
+
+      text-shadow: 0 0 3px;
+
+      outline: none;
+    }
+  }
+
+  .green-flash {
+    $green-color: #00FFCA;
+    color: $green-color; 
+
+    text-shadow: 0 0 10px $green-color;
+  }
+
+  .blue-flash {
+    $blue-color: #1f2799;
+    color: $blue-color;
+
+    text-shadow: 0 0 10px $blue-color;
+  }
+
+  .yellow-flash {
+    $yellow-color: #FFC857;
+    color: $yellow-color;
+
+    text-shadow: 0 0 10px $yellow-color;
+  }
+</style>
+
+<div class="terminal box">
+  <p>Hello! I'm <span class="blue-flash">JatoMixo</span>, a High School Student who loves <span class="yellow-flash">programming</span> stuff</p>
+  <p>Type <span class="green-flash">help</span> to know more about me</p>
+  <div class="row">
+    <p style="font-size: 150%; color: {lineStatus};">{leftArrow}</p>
+    <input type="text" bind:value={command}>
+  </div>
+</div>
