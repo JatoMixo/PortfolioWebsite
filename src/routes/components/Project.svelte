f10119ff09a5e92edfec38207a3d363f06c0bfe8
JatoMixo <jatomixo@outlook.es>
2023-05-09 16:40:15 +0200

    Continued working on project component

--- src/routes/components/Project.svelte
+++ src/routes/components/Project.svelte
@@ -26,13 +26,7 @@
   }
 
   .inner-box {
-    margin-right: 0;
-    margin-top: 0;
-
-    transform: translate(0.25vw, -0.5vh);
-
-    position: relative;
-    float: right;
+    width: fit-content;
   }
 
   h1 {
@@ -43,6 +37,36 @@
   p {
     margin-left: $margin-left;
     margin-top: 0;
+  }
+
+  img {
+    height: 6dvh;
+  }
+
+  .github-element {
+    height: 6vh;
+    justify-content: center;
+
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    font-size: 125%;
+
+    p {
+      margin-top: 0;
+
+      transform: translateY(50%);
+    }
+  }
+
+  a {
+    color: #E0E0E0;
+
+    :hover {
+      color: white;
+    }
+
+    text-decoration: none;
   }
 </style>
 
@@ -55,8 +79,10 @@
   </div>
   <p>{description}</p>
   <hr>
-  <div class="row">
-    <img src={GithubLogo} alt="Didn't load">
-    <p>GitHub</p>
-  </div>
+  <a href={github} target="_blank">
+    <div class="github-element row">
+      <img src={GithubLogo} alt="Didn't load">
+      <p>GitHub</p>
+    </div>
+  </a>
 </div>
