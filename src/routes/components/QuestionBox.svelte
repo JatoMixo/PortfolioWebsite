38bbeb7f0a1fc623cd53363d9ada646b31816c26
JatoMixo <jatomixo@outlook.es>
2023-06-24 16:19:34 +0200

    Corrected grammar in a question and fixed shadow not working on question title

--- src/routes/components/QuestionBox.svelte
+++ src/routes/components/QuestionBox.svelte
@@ -3,14 +3,12 @@
     let actual_question = "";
 
     const questions = {
-        "Why did you learn to code": "Mainly, because I wanted to make my own games, and that's the reason I started with Unity",
+        "Why did you learn to code": "Mainly because I wanted to make my own games, and that's the reason I started with Unity",
         "How did you start learning": "I started by watching programming tutorials on youtube and started making projects as soon as I could",
     };
 
     function changeQuestion(newQuestion) {
         actual_question = newQuestion;
-
-        console.log(actual_question);
     }
 </script>
 
@@ -58,7 +56,7 @@
 
         font-size: 150%;
         color: white;
-        text-shadow: 0 0 10px whtie;
+        text-shadow: 0 0 10px white;
     }
 
     .question-body {
